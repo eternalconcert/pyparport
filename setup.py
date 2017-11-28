4456065100690f14dc42aa7645621347f86ea567
Christian-Desktop <devnull@localhost>
2017-11-28 23:05:45 +0100

    Typo in description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ extension = Extension("_interface", sources=["_interface/pyparport.c"])
 
 setup(name="pyparport",
       version="0.6",
-      description="This module provides the possibility to connect to the first parallel port from Python.",
+      description="This module provides the possibility to connect to parallel ports from Python.",
       license='GPLv3',
       packages=['_interface', "pyparport"],
       author='Christian Kokoska',
